a3838eacdc804ca0a71d992e3f0368d90eb4fc26
Pranav Shinde <pranavshinde.as@gmail.com>
2024-11-27 11:22:48 +0530

    printing patter using loops - conditional loop completed

--- Conditionals And Loops/PrintingPatternUsingLoops.c
+++ Conditionals And Loops/PrintingPatternUsingLoops.c
@@ -1,22 +1,32 @@
-// #include <stdio.h>
-// int main() 
-// {
+#include <stdio.h>
+int main() 
+{
 
-//     int n = 4;
+    int n;
+    scanf("%d", &n);
+     int size = 2 * n - 1;   
+    int start = 0;
+    int end = size - 1;
+    int array[size][size];
 
-//     // scanf("%d", &n);
-//     // we used 2n-1 to match the size of rows and columns 
-//   	for (int i = 0; i<2*n-1; i++) {
-//       for (int j=0; j<2*n-1; j++) {
-//           if(i == 0 ||j==0 || j == 2 * n -2 || i == 2 * n -2){
-//               printf("%d ", n);
-//           }
-//           else if(){
-//             printf("%d ", n-1);
-//           }
-//       }
-//       printf("\n");
-      
-//       }
-//     return 0;
-// }
+    while(n!=0){
+      for(int i=start; i<=end; i++){
+        for (int j=start; j<=end; j++){
+          if(i == start || i == end || j == start || j == end){
+            array[i][j] = n;
+          }
+        }
+      }
+        ++start;
+        --end;
+        --n;
+    }
+    
+    for(int i = 0; i<size; i++){
+        for(int j = 0; j<size; j++){
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
